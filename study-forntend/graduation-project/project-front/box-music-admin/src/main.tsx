54143d2b787ee94d4e461b796956bb686a2e4d97
zhuxinxi <zhuxinxi@tuhu.cn>
2022-12-01 12:55:18 +0800

    202212011254-更新 antd 版本至 5.0

--- study-forntend/graduation-project/project-front/box-music-admin/src/main.tsx
+++ study-forntend/graduation-project/project-front/box-music-admin/src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import 'antd/dist/antd.css';
+// import 'antd/dist/antd.css';
 import 'nprogress/nprogress.css';
 
 import App from '@/App';
